1983d5670a463dc49f76e84504b240e985c07f97
JoeFrumenti <joefrumenti@gmail.com>
2025-11-29 09:06:03 -0800

    Added Gamepad Class

---
+++ SnakeSlime/MonoGameLibrary/Input/GamePadInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class GamePadInfo
+{
+
+}
--- SnakeSlime/MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
+++ SnakeSlime/MonoGameLibrary/obj/Debug/net8.0/MonoGameLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204c1dacc60d8e917c24119141d41e117e130028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f9c051948a56c3ade062f4fb1eab3e378ce5eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
